e9ec06a01f72944346cd0019b479730aa7c689b2
mbista25742 <mbista25742@ucumberlands.edu>
2024-10-26 17:29:02 +0000

    Final Submission

--- InsertionSort.py
+++ InsertionSort.py
@@ -3,7 +3,7 @@ arr = [5,2,9,1,7, 0,4,2,1,8,23,45,34]
 for i in range(1, len(arr)):
     current = arr[i]
     j = i-1
-    while j >=0 and arr[j] > current:
+    while j >=0 and arr[j] < current:
         arr[j+1] = arr[j]
         j = j-1
     arr[j+1] = current
